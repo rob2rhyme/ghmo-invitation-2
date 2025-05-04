d46d467deaf99fbc049275717c90868e78a28a4a
Robin G Thapa <61993378+rob2rhyme@users.noreply.github.com>
2025-05-04 13:06:16 -0400

    updated build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Raise threshold so you only get warned above 1 MB
+    chunkSizeWarningLimit: 1200,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        }
+      }
+    }
+  },
   server: {
     port: 3000,
-  },
-})
+    hmr: { overlay: false }
+  }
+});
