229cddd10574fccdf3b673bcd286dbfa508b92d5
Robin G Thapa <61993378+rob2rhyme@users.noreply.github.com>
2025-05-04 14:47:04 -0400

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Raise threshold so you only get warned above 1 MB
-    chunkSizeWarningLimit: 1200,
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
+          if (id.includes('node_modules/react')) {
+            return 'react'; // All React-related code goes into a separate chunk
+          }
+          if (id.includes('node_modules/lodash')) {
+            return 'lodash'; // All Lodash-related code goes into a separate chunk
           }
         }
       }
